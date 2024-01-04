cab06dd6991ae837dc4f72f5f988c7bc6b6a24be
VadimDomkov <vdomkov@yandex.ru>
2024-01-04 18:01:30 +0300

    fix : after review

--- src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.validation.ValidationException;
-
 @RestControllerAdvice
 public class ErrorHandler {
 
